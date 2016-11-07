927c49d388bf97f671819d910bebbe60d2d93428
borakrc <borakrc@gmail.com>
2016-11-08 01:50:15 +0200

    Shuttle Schedule is updated

--- Shuttle/DayBusses.py
+++ Shuttle/DayBusses.py
@@ -6,33 +6,24 @@ class DayBusses(object):
     def getMock(weekDay):
         dailyBuses = []
 
+        if weekDay <= 7:
+            for hour in [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]:
+                bus = {'index': 0, 'price': 0.90, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
+                       'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"},
+                                 {"hour": hour, "minute": 10, "tr_name": "Muze", "en_name": "Museum"},
+                                 {"hour": hour, "minute": 15, "tr_name": "Kopru Durak", "en_name": "Bridge Stop"},
+                                 {"hour": hour, "minute": 20, "tr_name": "ODTU Cemberi", "en_name": "METU Roundabout"}]}
+                dailyBuses.append(bus)
+
+                bus = {'index': 0, 'price': 0.90, 'tr_destination': 'Terminal', 'en_destination': 'Terminal',
+                       'stops': [{"hour": hour, "minute": 45, "tr_name": "2. Yurt", "en_name": "2nd Dormitory"},
+                                 {"hour": hour, "minute": 50, "tr_name": "ODTU Cemberi", "en_name": "METU Roundabout"},
+                                 {"hour": hour, "minute": 55, "tr_name": "Kopru Durak", "en_name": "Bridge Stop"},
+                                 {"hour": hour + 1, "minute": 00, "tr_name": "Muze", "en_name": "Museum"}
+                                 ]}
+                dailyBuses.append(bus)
         if weekDay <= 6:
-            for hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]:
-                bus = {'index': 0, 'price': 0.90, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
-                       'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"},
-                                 {"hour": hour, "minute": 10, "tr_name": "Muze", "en_name": "Museum"},
-                                 {"hour": hour, "minute": 15, "tr_name": "Kopru Durak", "en_name": "Bridge Stop"},
-                                 {"hour": hour, "minute": 20, "tr_name": "ODTU Cemberi", "en_name": "METU Roundabout"}]}
-                dailyBuses.append(bus)
-
-                bus = {'index': 0, 'price': 0.90, 'tr_destination': 'Terminal', 'en_destination': 'Terminal',
-                       'stops': [{"hour": hour, "minute": 45, "tr_name": "2. Yurt", "en_name": "2nd Dormitory"},
-                                 {"hour": hour, "minute": 50, "tr_name": "ODTU Cemberi", "en_name": "METU Roundabout"},
-                                 {"hour": hour, "minute": 55, "tr_name": "Kopru Durak", "en_name": "Bridge Stop"},
-                                 {"hour": hour + 1, "minute": 00, "tr_name": "Muze", "en_name": "Museum"}
-                                 ]}
-                """if hour == 17:
-                    bus['stops'] = [{"hour": hour + 1, "minute": 05, "tr_name": "2. Yurt", "en_name": "2nd Dormitory"},
-                                    {"hour": hour + 1, "minute": 10, "tr_name": "ODTU Cemberi",
-                                     "en_name": "METU Roundabout"},
-                                    {"hour": hour + 1, "minute": 15, "tr_name": "Kopru Durak",
-                                     "en_name": "Bridge Stop"},
-                                    {"hour": hour + 1, "minute": 20, "tr_name": "Muze", "en_name": "Museum"}
-                                    ]"""
-                dailyBuses.append(bus)
-
-        elif weekDay == 7:
-            for hour in [10, 13, 15, 17, 19, 23]:
+            for hour in [8, 9]:
                 bus = {'index': 0, 'price': 0.90, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
                        'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"},
                                  {"hour": hour, "minute": 10, "tr_name": "Muze", "en_name": "Museum"},
@@ -47,5 +38,5 @@ class DayBusses(object):
                                  {"hour": hour + 1, "minute": 00, "tr_name": "Muze", "en_name": "Museum"}
                                  ]}
                 dailyBuses.append(bus)
-
+                
         return dailyBuses
