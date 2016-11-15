907bc63678c38d0bcc970774508928cc06499972
borakrc <borakrc@gmail.com>
2016-11-15 09:10:04 +0300

    variable name-library name conflict in config.py is fixed in bartu's code.

--- Config.py
+++ Config.py
@@ -1,28 +1,28 @@
 from Helpers.DbCredentials import DbCredentials
-import os
+from os import path
 
 
 class Config:
     serverType = "Deployment"
-    home = os.path.dirname(__file__)
+    home = path.dirname(__file__)
     if serverType == 'Test':
         os = 'Windows'
         debug = True
         serverIp = "127.0.0.1"
-        staticFolderPath = os.path.join(home, "imageUploads")
-        dynamicFolderPath = os.path.join(home, "dynamicImages")
-        loggerPath = os.path.join(home, 'log.txt')
-        cafeteriaMenuExcelPath = os.path.join(
+        staticFolderPath = path.join(home, "imageUploads")
+        dynamicFolderPath = path.join(home, "dynamicImages")
+        loggerPath = path.join(home, 'log.txt')
+        cafeteriaMenuExcelPath = path.join(
                 home, 'Cafeteria', 'cafeteriaMenu.xlsx'
         )
     else:
         os = 'Linux'
         debug = False
         serverIp = "144.122.156.67"
-        staticFolderPath = "/home/ncc-mobileapp/metumobile/imageUploads/"
-        dynamicFolderPath = "/home/ncc-mobileapp/metumobile/dynamicImages/"
+        staticFolderPath = "/home/ncc-mobileapp/metumobile2/imageUploads/"
+        dynamicFolderPath = "/home/ncc-mobileapp/metumobile2/dynamicImages/"
         loggerPath = 'log.txt'
-        cafeteriaMenuExcelPath = '/home/ncc-mobileapp/metumobile/Cafeteria/cafeteriaMenu.xlsx'
+        cafeteriaMenuExcelPath = '/home/ncc-mobileapp/metumobile2/Cafeteria/cafeteriaMenu.xlsx'
 
     serverPort = 1072
     serverRootLink = "http://" + serverIp + ":" + str(serverPort)
