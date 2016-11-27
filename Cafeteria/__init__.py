1780c796a46773d38728dd4008c660a2a4831a38
borakrc <borakrc@gmail.com>
2016-11-28 01:13:09 +0300

    cafeteriaMenu get method bugfix

--- Cafeteria/__init__.py
+++ Cafeteria/__init__.py
@@ -2,7 +2,7 @@ class Cafeteria:
     def __init__(self):
         pass
 
-    def getSchedule(self, version=1.0):
+    def getSchedule(self, version):
         from MongoDatabase import MongoDatabase
         return MongoDatabase().getCafeteriaMenu(version)
 
