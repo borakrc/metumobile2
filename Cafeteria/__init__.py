aebd258ef98259343b6ff2857b0011c9f78cc3b5
borakrc <borakrc@gmail.com>
2016-12-26 05:01:24 +0300

    database adapter for getMeal(mealid)

--- Cafeteria/__init__.py
+++ Cafeteria/__init__.py
@@ -1,14 +1,28 @@
+from MongoDatabase import MongoDatabase
+
+
 class Cafeteria:
     def __init__(self):
         pass
 
     def getUpcomingSchedule(self, version):
-        from MongoDatabase import MongoDatabase
         return MongoDatabase().getUpcomingCafeteriaMenu(version if version >= 1.1 else 1.0)
 
     def getAllMeals(self):
-        from MongoDatabase import MongoDatabase
         return MongoDatabase().getAllCafeteriaMenu()
+
+    def expandRatingsWithMeal(self, mealRatings):
+        for meal in mealRatings:
+            mealId = meal['_id']
+            mealDetails = MongoDatabase().getMeal(mealId=mealId)
+            meal['tr_name'] = mealDetails['tr_name']
+            meal['dishes.main.tr_name'] = mealDetails['dishes']['main']['tr_name']
+            meal['dishes.side.tr_name'] = mealDetails['dishes']['side']['tr_name']
+            meal['dishes.soup.tr_name'] = mealDetails['dishes']['soup']['tr_name']
+
+        return mealRatings
+
+
 
 
 
