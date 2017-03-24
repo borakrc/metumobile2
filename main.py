32b737d2dcb4c01f59cd68760689ddaf73dfe503
borakrc <borakrc@gmail.com>
2017-03-24 15:54:33 +0300

    all caches dynamic + master cache

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def cacheVersionOf(data):
 
 def readFromWeb(url):
     import urllib
-    return urllib.urlopen(url).read()
+    return str(urllib.urlopen(url).read())
 
 # BEG MENU UPLOAD_______
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
