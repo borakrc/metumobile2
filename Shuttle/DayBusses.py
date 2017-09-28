6adc2300dbcac3ff828da4b413662d8d79fa2e25
aralzaim7 <aral@metu.edu.tr>
2017-09-28 11:42:10 +0300

    weekday ile olan karışıklık çözüldü
    Bütün günlerde aynı otobüs vakitleri olduğundan kod ona göre düzenlendi ve pazar günü için ayrı yazılan kod silindi.

--- Shuttle/DayBusses.py
+++ Shuttle/DayBusses.py
@@ -6,7 +6,7 @@ class DayBusses(object):
     def getMock(weekDay):
         dailyBuses = []
 
-        if weekDay <= 6:
+        if weekDay <= 7:
             for hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 00]:
                 bus = {'index': 0, 'price': 0.95, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
                        'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"},
@@ -17,25 +17,6 @@ class DayBusses(object):
                 dailyBuses.append(bus)
 
                 bus = {'index': 0, 'price': 0.95, 'tr_destination': 'Terminal', 'en_destination': 'Terminal',
-                   'stops': [{"hour": hour, "minute": 45, "tr_name": "2. Yurt", "en_name": "2nd Dormitory"},
-                             {"hour": hour, "minute": 50, "tr_name": "Kalkanli", "en_name": "Kalkanli"},
-                             {"hour": hour, "minute": 55, "tr_name": "ODTU Cemberi", "en_name": "METU Roundabout"},
-                             {"hour": hour + 1, "minute": 00, "tr_name": "Kopru Durak", "en_name": "Bridge Stop"},
-                             {"hour": hour + 1, "minute": 05, "tr_name": "Muze", "en_name": "Museum"}
-                             ]}
-                dailyBuses.append(bus)
-
-        if weekDay > 6:
-            for hour in [10, 13, 15, 17, 19, 23]:
-                bus = {'index': 0, 'price': 1.0, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
-                       'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"},
-                                 {"hour": hour, "minute": 10, "tr_name": "Muze", "en_name": "Museum"},
-                                 {"hour": hour, "minute": 15, "tr_name": "Kopru Durak", "en_name": "Bridge Stop"},
-                                 {"hour": hour, "minute": 20, "tr_name": "ODTU Cemberi", "en_name": "METU Roundabout"},
-                                 {"hour": hour, "minute": 25, "tr_name": "Kalkanli", "en_name": "Kalkanli"}]}
-                dailyBuses.append(bus)
-
-                bus = {'index': 0, 'price': 1.00, 'tr_destination': 'Terminal', 'en_destination': 'Terminal',
                    'stops': [{"hour": hour, "minute": 45, "tr_name": "2. Yurt", "en_name": "2nd Dormitory"},
                              {"hour": hour, "minute": 50, "tr_name": "Kalkanli", "en_name": "Kalkanli"},
                              {"hour": hour, "minute": 55, "tr_name": "ODTU Cemberi", "en_name": "METU Roundabout"},
