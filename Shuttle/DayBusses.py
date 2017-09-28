ae5399f1c9a41035dfa0a64647fc7a94eed0f768
aralzaim7 <aral@metu.edu.tr>
2017-09-28 10:34:46 +0300

    Yeni Otobus Tarife vakitleri
    otobüs vakitlerinde değişiklik her gun ilk otobus 8.05 te terminalden kalkacak, ve son otobus 00.45 te okuldan kalkacak.

--- Shuttle/DayBusses.py
+++ Shuttle/DayBusses.py
@@ -6,9 +6,9 @@ class DayBusses(object):
     def getMock(weekDay):
         dailyBuses = []
 
-        if weekDay <= 5:
-            for hour in [8, 10, 13, 15, 17, 19, 23]:
-                bus = {'index': 0, 'price': 1.0, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
+        if weekDay <= 6:
+            for hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 00]:
+                bus = {'index': 0, 'price': 0.95, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
                        'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"},
                                  {"hour": hour, "minute": 10, "tr_name": "Muze", "en_name": "Museum"},
                                  {"hour": hour, "minute": 15, "tr_name": "Kopru Durak", "en_name": "Bridge Stop"},
@@ -16,7 +16,7 @@ class DayBusses(object):
                                  {"hour": hour, "minute": 25, "tr_name": "Kalkanli", "en_name": "Kalkanli"}]}
                 dailyBuses.append(bus)
 
-                bus = {'index': 0, 'price': 1.00, 'tr_destination': 'Terminal', 'en_destination': 'Terminal',
+                bus = {'index': 0, 'price': 0.95, 'tr_destination': 'Terminal', 'en_destination': 'Terminal',
                    'stops': [{"hour": hour, "minute": 45, "tr_name": "2. Yurt", "en_name": "2nd Dormitory"},
                              {"hour": hour, "minute": 50, "tr_name": "Kalkanli", "en_name": "Kalkanli"},
                              {"hour": hour, "minute": 55, "tr_name": "ODTU Cemberi", "en_name": "METU Roundabout"},
@@ -25,7 +25,7 @@ class DayBusses(object):
                              ]}
                 dailyBuses.append(bus)
 
-        if weekDay > 5:
+        if weekDay > 6:
             for hour in [10, 13, 15, 17, 19, 23]:
                 bus = {'index': 0, 'price': 1.0, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
                        'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"},
