ca481d73f3c897f48f81b2fc3978711238655f8c
aralzaim7 <aral@metu.edu.tr>
2018-01-12 10:12:02 +0200

    added depart_from to json

--- Shuttle/DayBusses2.py
+++ Shuttle/DayBusses2.py
@@ -8,12 +8,14 @@ class DayBusses2(object):
 
         if weekDay <= 7:
             for hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 00]:
-                bus = {'index': 0, 'price': 0.95, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC', 'departure_hour': hour,
+                bus = {'index': 0, 'price': 0.95, 'tr_depart_from' : 'Terminal', 'en _depart_from' : 'Terminal', 
+                       'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC', 'departure_hour': hour,
                        'departure_minute': 5, 'arrive_hour': hour, 'arrive_minute': 45 }
                       
                 dailyBuses.append(bus)
                 
-                bus = {'index': 0, 'price': 0.95, 'tr_destination': 'Terminal', 'en_destination': 'Terminal', 'departure_hour': hour,
+                bus = {'index': 0, 'price': 0.95, 'tr_depart_from' : 'ODTU KKK', 'en _depart_from' : 'METU NCC', 
+                       'tr_destination': 'Terminal', 'en_destination': 'Terminal', 'departure_hour': hour,
                        'departure_minute': 45, 'arrive_hour': hour+1 , 'arrive_minute': 5 }
                 dailyBuses.append(bus)
 
