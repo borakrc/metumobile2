07bd2ec02337578c9133a68dbbf97ba788872de2
aralzaim7 <aral@metu.edu.tr>
2018-01-12 14:12:30 +0200

    Update DayBusses2.py

--- Shuttle/DayBusses2.py
+++ Shuttle/DayBusses2.py
@@ -5,7 +5,7 @@ class DayBusses2(object):
     @staticmethod
     def getMock(dayType):
         dailyBuses = []
-        
+        dayType = (dayType - 1)
         if dayType == 0:
             for hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 00]:
                 bus = {'index': 0, 'price': 0.95, 'tr_depart_from' : 'Terminal', 'en _depart_from' : 'Terminal', 
@@ -28,4 +28,6 @@ class DayBusses2(object):
                        'tr_destination': 'Terminal', 'en_destination': 'Terminal', 'departure_hour': hour,
                        'departure_minute': 45, 'arrive_hour': hour+1 , 'arrive_minute': 5 }
                 dailyBuses.append(bus)
+            return dailyBuses
+        else:
             return dailyBuses
