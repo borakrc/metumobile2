ffef1293d35def291b462e8d74def154f2d22736
aralzaim7 <aral@metu.edu.tr>
2018-01-12 09:16:15 +0200

    Test Schedule changes

--- Shuttle/__init__.py
+++ Shuttle/__init__.py
@@ -1,4 +1,5 @@
 from Shuttle.DayBusses import DayBusses
+from Shuttle.DayBusses2 import DayBusses2
 
 
 class Shuttle:
@@ -13,3 +14,11 @@ class Shuttle:
             ShuttleSchedule[weekDay-1]['buses'] = DayBusses().getMock(weekDay)
 
         return ShuttleSchedule
+    @staticmethod
+    def getWeeklySchedule2():
+        ShuttleSchedule2 = []
+        for weekDay in range(1, 8):
+            ShuttleSchedule2.append({'day': weekDay})
+            ShuttleSchedule2[weekDay-1]['buses'] = DayBusses2().getMock(weekDay)
+
+        return ShuttleSchedule2
