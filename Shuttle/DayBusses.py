9e52dfb568906294629b9565908739885026fd28
aralzaim7 <aral@metu.edu.tr>
2018-01-22 12:23:00 +0200

    Tatil günleri otobüs saatleri güncellemesi

--- Shuttle/DayBusses.py
+++ Shuttle/DayBusses.py
@@ -7,7 +7,7 @@ class DayBusses(object):
         dailyBuses = []
 
         if weekDay <= 7:
-            for hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 00]:
+            for hour in [10, 12, 14, 16, 18, 20, 22, 23]:
                 bus = {'index': 0, 'price': 0.95, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
                        'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"},
                                  {"hour": hour, "minute": 10, "tr_name": "Muze", "en_name": "Museum"},
