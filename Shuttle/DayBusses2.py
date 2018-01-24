8f94eb1f6ff2c842f05b843f4b7011936e5f8e49
aralzaim7 <aral@metu.edu.tr>
2018-01-24 13:54:40 +0200

    Update DayBusses2.py

--- Shuttle/DayBusses2.py
+++ Shuttle/DayBusses2.py
@@ -7,7 +7,7 @@ class DayBusses2(object):
         dailyBuses = []
         
         if dayType == 0:
-             for hour in [10, 12, 14, 16, 18, 20, 22, 23]:
+            for hour in [10, 12, 14, 16, 18, 20, 22, 23]:
                 bus = {'index': 0, 'price': 0.95, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
                        'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"}]}
                 dailyBuses.append(bus)
