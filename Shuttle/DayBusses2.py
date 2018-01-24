01aeb7394aa62f92521c29b0c2e0474f7b4cdcd3
aralzaim7 <aral@metu.edu.tr>
2018-01-24 13:47:04 +0200

    update new json for shuttle

--- Shuttle/DayBusses2.py
+++ Shuttle/DayBusses2.py
@@ -7,26 +7,26 @@ class DayBusses2(object):
         dailyBuses = []
         
         if dayType == 0:
-            for hour in [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 00]:
-                bus = {'index': 0, 'price': 0.95, 'tr_depart_from' : 'Terminal', 'en _depart_from' : 'Terminal', 
-                       'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC', 'departure_hour': hour,
-                       'departure_minute': 5, 'arrive_hour': hour, 'arrive_minute': 45 }
+             for hour in [10, 12, 14, 16, 18, 20, 22, 23]:
+                bus = {'index': 0, 'price': 0.95, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
+                       'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"}]}
                 dailyBuses.append(bus)
-                bus = {'index': 0, 'price': 0.1, 'tr_depart_from' : 'ODTU KKK', 'en _depart_from' : 'METU NCC', 
-                       'tr_destination': 'Terminal', 'en_destination': 'Terminal', 'departure_hour': hour,
-                       'departure_minute': 45, 'arrive_hour': hour+1 , 'arrive_minute': 5 }
+
+                bus = {'index': 0, 'price': 0.95, 'tr_destination': 'Terminal', 'en_destination': 'Terminal',
+                   'stops': [{"hour": hour, "minute": 45, "tr_name": "2. Yurt", "en_name": "2nd Dormitory"}
+                             ]}
                 dailyBuses.append(bus)
             return dailyBuses
         
         elif dayType == 1:          
-            for hour in [8, 10, 12, 14, 16, 18, 20, 22, 00]:
-                bus = {'index': 0, 'price': 0.95, 'tr_depart_from' : 'Terminal', 'en _depart_from' : 'Terminal', 
-                       'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC', 'departure_hour': hour,
-                       'departure_minute': 5, 'arrive_hour': hour, 'arrive_minute': 45 }
+                         for hour in [10, 12, 14, 16, 18, 20, 22, 23]:
+                bus = {'index': 0, 'price': 0.95, 'tr_destination': 'ODTU KKK', 'en_destination': 'METU NCC',
+                       'stops': [{"hour": hour, "minute": 5, "tr_name": "Terminal", "en_name": "Terminal"}]}
                 dailyBuses.append(bus)
-                bus = {'index': 0, 'price': 0.95, 'tr_depart_from' : 'ODTU KKK', 'en _depart_from' : 'METU NCC', 
-                       'tr_destination': 'Terminal', 'en_destination': 'Terminal', 'departure_hour': hour,
-                       'departure_minute': 45, 'arrive_hour': hour+1 , 'arrive_minute': 5 }
+
+                bus = {'index': 0, 'price': 0.95, 'tr_destination': 'Terminal', 'en_destination': 'Terminal',
+                   'stops': [{"hour": hour, "minute": 45, "tr_name": "2. Yurt", "en_name": "2nd Dormitory"}
+                             ]}
                 dailyBuses.append(bus)
             return dailyBuses
         else:
