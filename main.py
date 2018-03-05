1e39c9966c9ff04fa6cc9a4435957b1121157858
Aral Zaim <aral@metu.edu.tr>
2018-03-05 14:33:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def cacheVersion():
 
 def cacheVersionOf(data):
     change=""
-    change=change+str(11)
+    change=change+str(12)
     data = str(data)+str(change)
     md5Hash = hashlib.md5(data).hexdigest()
     return jsonify(cacheVersion=md5Hash)
