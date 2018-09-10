298fbc30d2902edadfdf590d9888cdb04079f3e6
Aral Zaim <aral@metu.edu.tr>
2018-09-10 15:35:23 +0300

    self.connectionNew.commit() eklendi

--- Announcements/MetuAnnouncementsBridge.py
+++ Announcements/MetuAnnouncementsBridge.py
@@ -102,7 +102,7 @@ class MetuAcademicAndDormCalendarBridge:
 
             # connection is not autocommit by default. So you must commit to save
             # your changes.
-            self.connection.commit()
+            self.connectionNew.commit()
             result = cursor.fetchall()
 
             announcementsShorterThan6Months = []
