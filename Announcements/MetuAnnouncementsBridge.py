2e2d257b122af514207e2056de810b7e409ffc04
Aral Zaim <aral@metu.edu.tr>
2018-09-11 14:21:47 +0300

    Update MetuAnnouncementsBridge.py

--- Announcements/MetuAnnouncementsBridge.py
+++ Announcements/MetuAnnouncementsBridge.py
@@ -20,16 +20,6 @@ class MetuAcademicAndDormCalendarBridge:
         )
     def _connectNew(self):
        return self.credentialsNew
-       self.connection = pymysql.connect(
-            user=self.credentialsNew.user,
-            password=self.credentialsNew.password,
-            host=self.credentialsNew.ip,
-            port=self.credentialsNew.port,
-            db=self.credentialsNew.dbName,
-            charset='utf8',
-            cursorclass=pymysql.cursors.DictCursor
-        )
-
 
     def _fetchAll(self):
         self._connect()
