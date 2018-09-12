9c6caa259aabf2ebd047515505920d6f0b99ca1a
Aral Zaim <aral@metu.edu.tr>
2018-09-12 12:00:54 +0300

    connection established testing

--- Announcements/MetuAnnouncementsBridge.py
+++ Announcements/MetuAnnouncementsBridge.py
@@ -73,13 +73,37 @@ class MetuAcademicAndDormCalendarBridge:
         
     def _fetchAllNew(self):
         self._connectNew()
-        if self.connection.cursor():
-           connected = True
-           
-        else:
-           connected = False
-          
-        return connected
+        with self.connection.cursor() as cursor:
+            # Create a new record
+            sql = """
+            SELECT 
+            aktak_id, 
+            aktak_ilk_tarih, 
+            aktak_son_tarih, 
+            aktak_metin_tr, 
+            aktak_metin_en, 
+            aktak_donem, 
+            aktak_eklenme_tarihi, 
+            aktak_ekleyen_userid, 
+            aktak_ekleyen_ip
+            FROM ss_portal.akademik_takvim  
+            ORDER BY aktak_ilk_tarih
+            ASC
+                """
+            cursor.execute(sql)
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+           self.connection.commit()
+            result = cursor.fetchall()
+
+            #announcementsShorterThan6Months = []
+            #for each in result:
+                #if not self._isLongerThan6Months(each):
+                    #each['date_to'] = each['date_to'].isoformat()
+                    #each['date_from'] = each['date_from'].isoformat()
+                    #announcementsShorterThan6Months.append(each)
+            return result
         
 
     def fetchAcademicAnnouncements(self):
