b26141a8f453a8975ffc6ca57f67026612e16eb4
Aral Zaim <aral@metu.edu.tr>
2018-09-12 12:10:55 +0300

    Update MetuAnnouncementsBridge.py

--- Announcements/MetuAnnouncementsBridge.py
+++ Announcements/MetuAnnouncementsBridge.py
@@ -103,7 +103,7 @@ class MetuAcademicAndDormCalendarBridge:
                 #each['date_to'] = each['date_to'].isoformat()
                 #each['date_from'] = each['date_from'].isoformat()
                 announcementsShorterThan6Months.append(each)
-            return result
+            return announcementsShorterThan6Months
         
 
     def fetchAcademicAnnouncements(self):
