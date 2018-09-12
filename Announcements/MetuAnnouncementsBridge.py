8d50b9ffe09a93fbe86d0656d43b3976cf508527
Aral Zaim <aral@metu.edu.tr>
2018-09-12 16:53:57 +0300

    Update MetuAnnouncementsBridge.py

--- Announcements/MetuAnnouncementsBridge.py
+++ Announcements/MetuAnnouncementsBridge.py
@@ -95,7 +95,7 @@ class MetuAcademicAndDormCalendarBridge:
                 each['description'] = ""
                 each['isAllDay'] = False
                 
-                if each['date_from'].date() > datetime.today().date():
+                #if each['date_from'].date() > datetime.today().date():
                     each['date_from'] = str(each['date_from'])
                     if each['date_to'] is None:
                         each['date_to'] = each['date_from']
