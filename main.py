d2ec4865d0eb5b77995db9a1cf0e1379da815a60
Aral Zaim <aral@metu.edu.tr>
2018-09-25 16:58:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -376,7 +376,6 @@ if __name__ == "__main__":
     import logging
     handler = logging.FileHandler(Config.loggerPath)  # errors logged to this file
     handler.setLevel(logging.ERROR)  # log errors and above
-    handler.setLevel(logging.WARNING)  # log warning
 
     app.logger.addHandler(handler)  # attach the handler to the app's logger
 
