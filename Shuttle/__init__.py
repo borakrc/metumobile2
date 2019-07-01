b05afb0e52de4a19b0901b74629b7103f14bf1db
Aral Zaim <aral@metu.edu.tr>
2019-07-01 11:06:39 +0300

    Update __init__.py

--- Shuttle/__init__.py
+++ Shuttle/__init__.py
@@ -19,9 +19,10 @@ class Shuttle:
         ShuttleSchedule2 = []
         for dayType in range(1, 3):
              ShuttleSchedule2.append({'day': (dayType-1)}) 
-             ShuttleSchedule2[dayType-1]['buses']=(DayBusses2().getMock(dayType-1))
-            
              ShuttleSchedule2[0].append('warning_message': 1)
              ShuttleSchedule2[1].append('warning_message': 1)      
+             ShuttleSchedule2[dayType-1]['buses']=(DayBusses2().getMock(dayType-1))
+            
+            
                
         return ShuttleSchedule2
