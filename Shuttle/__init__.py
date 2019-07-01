da4a1525355eadad3d2c884e5aa1802656a09f2a
Aral Zaim <aral@metu.edu.tr>
2019-07-01 11:04:32 +0300

    warning message

--- Shuttle/__init__.py
+++ Shuttle/__init__.py
@@ -18,7 +18,10 @@ class Shuttle:
     def getWeeklySchedule2():
         ShuttleSchedule2 = []
         for dayType in range(1, 3):
-             ShuttleSchedule2.append({'day': (dayType-1)})
-             ShuttleSchedule2[dayType-1]['buses']=(DayBusses2().getMock(dayType-1))      
+             ShuttleSchedule2.append({'day': (dayType-1)}) 
+             ShuttleSchedule2[dayType-1]['buses']=(DayBusses2().getMock(dayType-1))
+            
+             ShuttleSchedule2[0].append('warning_message': 1)
+             ShuttleSchedule2[1].append('warning_message': 1)      
                
         return ShuttleSchedule2
