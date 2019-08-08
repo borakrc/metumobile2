8b9c98ca2fa826e7776f7b7b5e7455b922b5664d
Aral Zaim <aral@metu.edu.tr>
2019-08-08 10:13:52 +0300

    Update __init__.py

--- Brochures/__init__.py
+++ Brochures/__init__.py
@@ -19,6 +19,7 @@ class Brochures:
     def _getBrochureData(self):
         return [
     ["test.jpg", "türkçe_title","türkçe_link", "ingilizce_title","ingilizce_link"],
+    ["test2.jpg","tr2_title","tr2_link","ing2_title","ing2_link"]
     ]
     
     
