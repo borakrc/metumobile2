1d285bfad85bd3a9adcda65e300662bef0c4a6cb
Aral Zaim <aral@metu.edu.tr>
2019-08-23 15:59:17 +0300

    Update __init__.py

--- Brochures/__init__.py
+++ Brochures/__init__.py
@@ -28,9 +28,9 @@ class Brochures:
         return [
     ["Uluslararası ve A Level Öğrencileri İçin Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/2019-Orientation-Brochure.pdf", "Orientation Program for International and A Level Students", "https://ncc.metu.edu.tr/sites/default/files/2019-Orientation-Brochure.pdf"],
     ["YKS Öğrencileri İçin Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/2019-Oryantasyon-Brosuru.pdf", "Orientation Program for YKS Students", "https://ncc.metu.edu.tr/sites/default/files/2019-Oryantasyon-Brosuru.pdf"],
-    ["Uluslararası ve A Level Öğrencileri İçin Yapılacaklar Listesi", "https://guide.ncc.metu.edu.tr/check_list/#book/", "Check List International and A Level Students", "https://guide.ncc.metu.edu.tr/check_list/#book/"]
-    ["YKS Öğrencileri İçin Yapılacaklar Listesi", "https://ncc.metu.edu.tr/sites/default/files/Check-List-v4.pdf", "Check List for YKS Students", "https://ncc.metu.edu.tr/sites/default/files/Check-List-v4.pdf"]
-    ["İlk Yıl Öğrencileri İçin Broşür (İngilizce)", "https://guide.ncc.metu.edu.tr/new_comers_brochures", "Broshures For New Comers in English", "https://guide.ncc.metu.edu.tr/new_comers_brochures"]
+    ["Uluslararası ve A Level Öğrencileri İçin Yapılacaklar Listesi", "https://guide.ncc.metu.edu.tr/check_list/#book/", "Check List International and A Level Students", "https://guide.ncc.metu.edu.tr/check_list/#book/"],
+    ["YKS Öğrencileri İçin Yapılacaklar Listesi", "https://ncc.metu.edu.tr/sites/default/files/Check-List-v4.pdf", "Check List for YKS Students", "https://ncc.metu.edu.tr/sites/default/files/Check-List-v4.pdf"],
+    ["İlk Yıl Öğrencileri İçin Broşür (İngilizce)", "https://guide.ncc.metu.edu.tr/new_comers_brochures", "Broshures For New Comers in English", "https://guide.ncc.metu.edu.tr/new_comers_brochures"],
     ["İlk Yıl Öğrencileri İçin Broşür (Türkçe)", "https://guide.ncc.metu.edu.tr/ilk_yil_brosuru", "Broshures For New Comers in Turkish", "https://guide.ncc.metu.edu.tr/ilk_yil_brosuru"]
     ]
     
