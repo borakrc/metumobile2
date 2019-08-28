67c53756b8e9a5d8e0ecbd5ff0e5e3fda8ddd866
Aral Zaim <aral@metu.edu.tr>
2019-08-28 16:49:47 +0300

    Update __init__.py

--- Alacarte/__init__.py
+++ Alacarte/__init__.py
@@ -7,3 +7,27 @@ class Alacarte:
 
     def getUpcomingSchedule(self, version):
         return MongoDatabase().getUpcomingCafeteriaMenu(version if version >= 1.1 else 1.0)
+
+    def getAllMeals(self):
+        return MongoDatabase().getAllCafeteriaMenu()
+
+    def expandRatingsWithMeal(self, mealRatings):
+        for meal in mealRatings:
+            mealId = meal['_id']
+
+            import urllib, json
+            from Config import Config
+            url = Config.cafeteriaServiceUrl + "/meals/"+mealId
+            response = urllib.urlopen(url).read()
+            jsonEndpointData = json.loads(response)
+            mealDetails = jsonEndpointData
+            meal['tr_name'] = mealDetails['tr_name']
+            meal['dishes.main.tr_name'] = mealDetails['dishes']['main']['tr_name']
+            meal['dishes.side.tr_name'] = mealDetails['dishes']['side']['tr_name']
+            meal['dishes.soup.tr_name'] = mealDetails['dishes']['soup']['tr_name']
+
+        return mealRatings
+
+    def getMeal(self, mealId):
+        meal = MongoDatabase().getMeal(mealId=mealId)
+        return meal
