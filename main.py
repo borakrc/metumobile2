cf485eaf5ac57d9664dcf05f9b1ac809a34ffb23
Aral Zaim <aral@metu.edu.tr>
2019-08-28 15:22:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from flask import Flask, jsonify, request, render_template
 from Announcements import Announcements
 from Booklets import Booklets
 from Brochures import Brochures
-from Alacarte import Alacarte
-from Cafeteria import Cafeteria
 from Cafeteria.ExcelImport import ExcelImport
 from Cafeteria.Rating import CafeteriaRating
 from Config import Config
