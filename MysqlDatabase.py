786ccff0cc16254fb5783df8e572965cc5dd34b5
Aral Zaim <aral@metu.edu.tr>
2019-08-29 14:09:58 +0300

    Update MysqlDatabase.py

--- MysqlDatabase.py
+++ MysqlDatabase.py
@@ -21,33 +21,7 @@ class MysqlDatabase:
             charset='utf8',
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True)
-        self.cursor = self.connection.cursor()
-        
-    def getUpcomingAlacarteMenu(self, version):
-        from datetime import datetime
-        
-        sql="select * from alacarte_menu where end_date > NOW()"
-        
-        myresult=self.cursor.execute(sql)
-        myresult = self.cursor.fetchall()
-       
-        jsonableArray = []
-        for each in myresult:
-
-            meal['id'] = str(each['id'])
-            meal['tr_type'] = str(each['tr_type'])
-            meal['en_type'] = str(each['en_type'])
-            meal['start_date'] = each['start_date'].isoformat()
-            meal['end_date'] = each['end_date'].isoformat()
-            meal['tr_name'] = str(each['tr_name'])
-            meal['en_name'] = str(each['en_name'])
-            meal['calorie'] = str(each['calorie'])
-            meal['protein'] = str(each['protein'])
-            meal['food_type'] = str(each['food_type'])
-            jsonableArray.append(meal)
-
-        return jsonableArray
-        
+        self.cursor = self.connection.cursor()        
         
     def setCafeteriaMenu(self, mealArray):
         for eachMeal in mealArray:
