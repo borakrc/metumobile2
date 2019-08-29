c286190a5bd770a027d028eb4ab99966917276a6
Aral Zaim <aral@metu.edu.tr>
2019-08-29 14:12:52 +0300

    Create AlacarteBridge.py

---
+++ Alacarte/AlacarteBridge.py
@@ -0,0 +1,44 @@
+import pymysql.cursors
+
+from CredentialsConfig import CredentialsConfig
+
+
+class AlacarteMenuBridge:
+    def __init__(self):
+        self.credentials = CredentialsConfig.alacarteRestaurantCredentials
+
+    def _connect(self):
+        self.connection = pymysql.connect(
+            user=self.credentials.user,
+            password=self.credentials.password,
+            host=self.credentials.ip,
+            db=self.credentials.dbName,
+            charset='utf8',
+            cursorclass=pymysql.cursors.DictCursor)
+
+    def getUpcomingAlacarteMenu(self, version):
+        from datetime import datetime
+        
+        sql="select * from alacarte_menu where end_date > NOW()"
+        
+        myresult=self.cursor.execute(sql)
+        myresult = self.cursor.fetchall()
+       
+        jsonableArray = []
+        for each in myresult:
+
+            meal['id'] = str(each['id'])
+            meal['tr_type'] = str(each['tr_type'])
+            meal['en_type'] = str(each['en_type'])
+            meal['start_date'] = each['start_date'].isoformat()
+            meal['end_date'] = each['end_date'].isoformat()
+            meal['tr_name'] = str(each['tr_name'])
+            meal['en_name'] = str(each['en_name'])
+            meal['calorie'] = str(each['calorie'])
+            meal['protein'] = str(each['protein'])
+            meal['food_type'] = str(each['food_type'])
+            jsonableArray.append(meal)
+
+        return jsonableArray
+
+
