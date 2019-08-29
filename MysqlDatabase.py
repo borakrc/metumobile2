4e893cff4d24d1b347805db1d24e9728dba3b334
Aral Zaim <aral@metu.edu.tr>
2019-08-29 13:51:16 +0300

    Update MysqlDatabase.py

--- MysqlDatabase.py
+++ MysqlDatabase.py
@@ -24,36 +24,31 @@ class MysqlDatabase:
         self.cursor = self.connection.cursor()
         
     def getUpcomingAlacarteMenu(self, version):
-        if version == 1.0:
-            from datetime import datetime
-            results = self.cursor['alacarte_menu'].find({"end_date": {"$gt": datetime.now()}})
-            jsonableArray = []
-            for each in results:
-                
-                meal['id'] = str(each['id'])
-                meal['tr_type'] = str(each['tr_type'])
-                meal['en_type'] = str(each['en_type'])
-                meal['start_date'] = each['start_date'].isoformat()
-                meal['end_date'] = each['end_date'].isoformat()
-                meal['tr_name'] = str(each['tr_name'])
-                meal['en_name'] = str(each['en_name'])
-                meal['calorie'] = str(each['calorie'])
-                meal['protein'] = str(each['protein'])
-                meal['food_type'] = str(each['food_type'])
-                jsonableArray.append(meal)
-                
-            return jsonableArray
-        else:
-            from datetime import datetime
-            #results = self.db['alacarte_menu'].find({})
-            results = self.db['alacarte_menu'].find({"end_date": {"$gt": datetime.now()}})
-            jsonableArray = []
-            for each in results:
-                each['end_date'] = each['end_date'].isoformat()
-                each['start_date'] = each['start_date'].isoformat()
-                each['id'] = str(each['id'])
-                jsonableArray.append(each)
-            return jsonableArray
+        from datetime import datetime
+        mycursor = mydb.cursor()
+
+        sql="select * from alacarte_menu where end_date > NOW()"
+        
+        myresult=self.cursor.execute(sql)
+        myresult = mycursor.fetchall()
+       
+        jsonableArray = []
+        for each in myresult:
+
+            meal['id'] = str(each['id'])
+            meal['tr_type'] = str(each['tr_type'])
+            meal['en_type'] = str(each['en_type'])
+            meal['start_date'] = each['start_date'].isoformat()
+            meal['end_date'] = each['end_date'].isoformat()
+            meal['tr_name'] = str(each['tr_name'])
+            meal['en_name'] = str(each['en_name'])
+            meal['calorie'] = str(each['calorie'])
+            meal['protein'] = str(each['protein'])
+            meal['food_type'] = str(each['food_type'])
+            jsonableArray.append(meal)
+
+        return jsonableArray
+        
         
     def setCafeteriaMenu(self, mealArray):
         for eachMeal in mealArray:
