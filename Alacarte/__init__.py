b5b0f058e07d50ac9985b8ff1395199149075337
Aral Zaim <aral@metu.edu.tr>
2019-08-29 10:54:14 +0300

    Update __init__.py

--- Alacarte/__init__.py
+++ Alacarte/__init__.py
@@ -5,4 +5,5 @@ class Alacarte:
         pass
     
     def getUpcomingAlacarte(self, version):
-        return version
+        return MysqlDatabase().getUpcomingAlacarteMenu(version if version >= 1.1 else 1.0)
+
