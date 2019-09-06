66fd76caead0cfea4f283fb5a9bc99547735ca2c
Aral Zaim <aral@metu.edu.tr>
2019-09-06 16:12:20 +0300

    Update AlacarteBridge.py

--- Alacarte/AlacarteBridge.py
+++ Alacarte/AlacarteBridge.py
@@ -21,29 +21,65 @@ class AlacarteBridge:
     def getUpcomingAlacarteMenu(self, version):
         self._connect()
         with self.connection.cursor() as cursor:
-            sql = "select * from alacarte_menu where end_date > NOW()"
+            sql = "select distinc end_date from alacarte_menu where end_date > NOW() order by end_date asc"
            
             cursor.execute(sql)
             
             self.connection.commit()
-            myresult = cursor.fetchall()
+            all_dates = cursor.fetchall()
         
         #self.cursor.execute("select * from alacarte_menu where end_date > NOW()")
 
         jsonableArray = []
-        for each in myresult:
-            meal = {}
-            meal['id'] = str(each['id'])
-            meal['tr_type'] = each['tr_type']
-            meal['en_type'] = each['en_type']
-            meal['start_date'] = each['start_date']
-            meal['end_date'] = each['end_date']
-            meal['tr_name'] = each['tr_name']
-            meal['en_name'] = each['en_name']
-            meal['calorie'] = each['calorie']
-            meal['protein'] = each['protein']
-            meal['food_type'] = each['food_type']
-            jsonableArray.append(meal)
+        for each_date in all_dates:
+            
+            dates = {}
+            dates['end_date'] = each_date['end_date']
+            
+            sql = "select * from alacarte_menu where end_date =" + each_date['end_date'] + " and en_type='Lunch' order by end_date asc"
+           
+            cursor.execute(sql)
+            
+            self.connection.commit()
+            all_lunch = cursor.fetchall()
+            
+            for each_lunch in all_lunch
+                lunches={}
+                
+                if(each_lunch['food_type'] == 'soup') 
+                    lunches['soup']['tr_name']=each_lunch['tr_name']
+                    lunches['soup']['en_name']=each_lunch['en_name']
+                    lunches['soup']['calorie']=each_lunch['calorie']
+                    lunches['soup']['protein']=each_lunch['protein']
+                if(each_lunch['food_type'] == 'main1') 
+                    lunches['main1']['tr_name']=each_lunch['tr_name']
+                    lunches['main1']['en_name']=each_lunch['en_name']
+                    lunches['main1']['calorie']=each_lunch['calorie']
+                    lunches['main1']['protein']=each_lunch['protein']
+                if(each_lunch['food_type'] == 'main2') 
+                    lunches['main2']['tr_name']=each_lunch['tr_name']
+                    lunches['main2']['en_name']=each_lunch['en_name']
+                    lunches['main2']['calorie']=each_lunch['calorie']
+                    lunches['main2']['protein']=each_lunch['protein']    
+                    
+                jsonableArray.append(lunches)  
+                    
+                
+            
+            
+            
+            #meal = {}
+            #meal['id'] = str(each['id'])
+            #meal['tr_type'] = each['tr_type']
+            #meal['en_type'] = each['en_type']
+            #meal['start_date'] = each['start_date']
+            #meal['end_date'] = each['end_date']
+            #meal['tr_name'] = each['tr_name']
+            #meal['en_name'] = each['en_name']
+            #meal['calorie'] = each['calorie']
+            #meal['protein'] = each['protein']
+            #meal['food_type'] = each['food_type']
+            #jsonableArray.append(meal)
 
         return jsonableArray
     
@@ -57,5 +93,3 @@ class AlacarteBridge:
 
                 self.connection.commit()
     
-
-
