ff7e7aab678b98e758fa3d1f8617f94f153dee19
Aral Zaim <aral@metu.edu.tr>
2019-09-27 11:14:27 +0300

    Update SiteMap.py

--- Helpers/SiteMap.py
+++ Helpers/SiteMap.py
@@ -29,8 +29,6 @@ class SiteMap:
 <li>clickable: <a href=\"/\">/</a></li>
 <li>clickable: <a href=\"/announcements/category/0\">/announcements/category</a></li>
 <li>clickable: <a href=\"/cafeteria/rating/\">POST: /cafeteria/rating/</a></li>
-<li>clickable: <a href=\"/services/cafeteria/excelupload\">Excel Tabledot Menu Upload</a></li>
-<li>clickable: <a href=\"/services/alacarte/excelupload\">Excel Alacarte Menu Upload</a></li>
 
 """
 
