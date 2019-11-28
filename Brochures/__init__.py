86cc9fe2cd8ffd5bd9e3a125d274fac1c3d66a5c
Aral Zaim <aral@metu.edu.tr>
2019-11-28 17:01:23 +0200

    Update __init__.py

--- Brochures/__init__.py
+++ Brochures/__init__.py
@@ -27,10 +27,10 @@ class Brochures:
     def _getBrochureData(self):
         return [
     ["Yüksek Lisans Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_for_graudate.pdf", "Orientation Program for Graduate Students", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_for_graudate.pdf"],
-    ["Uluslararası ve A Level Öğrencileri İçin Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_eng2019-v2.pdf", "Orientation Program for Int. and A Level Stds.", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_eng2019-v2.pdf"],
-    ["YKS Öğrencileri İçin Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_trv4.pdf", "Orientation Program for YKS Students", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_trv4.pdf"],
+    ["Uluslararası ve A Level Öğrencileri İçin Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/orientation_february.pdf", "Orientation Program for Int. and A Level Stds.", "https://ncc.metu.edu.tr/sites/default/files/orientation_february.pdf"],
+    #["YKS Öğrencileri İçin Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_trv4.pdf", "Orientation Program for YKS Students", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_trv4.pdf"],
     ["Uluslararası ve A Level Öğrencileri İçin Yapılacaklar Listesi", "https://guide.ncc.metu.edu.tr/check_list/#book/", "Check List International and A Level Students", "https://guide.ncc.metu.edu.tr/check_list/#book/"],
-    ["YKS Öğrencileri İçin Yapılacaklar Listesi", "https://ncc.metu.edu.tr/sites/default/files/Check-List-v4.pdf", "Check List for YKS Students", "https://ncc.metu.edu.tr/sites/default/files/Check-List-v4.pdf"],
+    #["YKS Öğrencileri İçin Yapılacaklar Listesi", "https://ncc.metu.edu.tr/sites/default/files/Check-List-v4.pdf", "Check List for YKS Students", "https://ncc.metu.edu.tr/sites/default/files/Check-List-v4.pdf"],
     ["İlk Yıl Öğrencileri İçin Broşür (İngilizce)", "https://guide.ncc.metu.edu.tr/new_comers_brochures", "Broshures For New Comers in English", "https://guide.ncc.metu.edu.tr/new_comers_brochures"],
     ["İlk Yıl Öğrencileri İçin Broşür (Türkçe)", "https://guide.ncc.metu.edu.tr/ilk_yil_brosuru", "Broshures For New Comers in Turkish", "https://guide.ncc.metu.edu.tr/ilk_yil_brosuru"]
     ]
