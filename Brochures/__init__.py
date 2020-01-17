8a93d69b8d4d652a0c76449dd6c31569f8debd78
Aral Zaim <aral@metu.edu.tr>
2020-01-17 16:39:07 +0200

    orientation programme for graduate programmes add

--- Brochures/__init__.py
+++ Brochures/__init__.py
@@ -26,7 +26,7 @@ class Brochures:
     
     def _getBrochureData(self):
         return [
-    #["Yüksek Lisans Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_for_graudate.pdf", "Orientation Program for Graduate Students", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_for_graudate.pdf"],
+    ["Yüksek Lisans Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_yukseklisans.pdf", "Orientation Program for Graduate Students", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_yukseklisans.pdf"],
     ["Uluslararası ve A Level Öğrencileri İçin Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/orientation_february.pdf", "Orientation Program for Int. and A Level Stds.", "https://ncc.metu.edu.tr/sites/default/files/orientation_february.pdf"],
     #["YKS Öğrencileri İçin Oryantasyon Programı", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_trv4.pdf", "Orientation Program for YKS Students", "https://ncc.metu.edu.tr/sites/default/files/oryantaston_trv4.pdf"],
     ["Uluslararası ve A Level Öğrencileri İçin Yapılacaklar Listesi", "https://ncc.metu.edu.tr/sites/default/files/Checklist-Spring2020.pdf", "Check List International and A Level Students", "https://ncc.metu.edu.tr/sites/default/files/Checklist-Spring2020.pdf"],
